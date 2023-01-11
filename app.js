d70045e88d7bb58ca5e12f24b45ac0d2af5a7c14
Rohan-1-3 <rohanshrestha130@gmail.com>
2023-01-11 09:07:20 +0545

    Added CSS for bookInputContainer and added JS for adding new book in HTML file

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const mainContent = document.querySelector(".main-content")
 const container = document.getElementById("main-container");
+// adding elements for bookInputContainer
+const addBook = document.createElement("div");
+const addBookForm = document.createElement("form");
+const titleLabel = document.createElement("label");
+const titleInput = document.createElement("input");
+const authorLabel = document.createElement("label");
+const authorInput = document.createElement("input");
+const pagesLabel = document.createElement("label");
+const pagesInput = document.createElement("input");
+const readStatusLabel = document.createElement("label");
+const readStatusInput = document.createElement("input");
+const bookAddSubmit = document.createElement("button");
+const addButton = document.querySelector(".add-book");// getting the add button
 
 // Constructor for getting info on book
 function Book(title, author, pages, status){
@@ -10,9 +23,9 @@ function Book(title, author, pages, status){
 }
 // get info of book from the user
 function addBookToLibrary(){
-    const title = prompt("Enter");
-    const author = prompt("Enter");
-    const pages = prompt("Enter");
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
     const status = prompt("Enter");
     const newBook = new Book(title, author, pages, status)
     added(newBook);// calling constructor 
@@ -47,29 +60,15 @@ function added(newBook){
     newDiv.appendChild(removeButton);
 }
 
+
+
 function add(){
-    const addButton = document.querySelector(".add-book");// getting the add button
 
     // setting CSS grid for main content and bookInputContainer
     container.style.display = "grid";
     container.style.gridTemplateColumns = "8fr 2fr";
     container.style.backgroundColor = "green";
 
-    // adding elements for bookInputContainer
-    const addBook = document.createElement("div");
-    const addBookForm = document.createElement("form");
-    const titleLabel = document.createElement("label");
-    const titleInput = document.createElement("input");
-    const titlePara = document.createElement("p");
-    const authorLabel = document.createElement("label");
-    const authorInput = document.createElement("input");
-    const authorPara = document.createElement("p");
-    const pagesLabel = document.createElement("label");
-    const pagesInput = document.createElement("input");
-    const pagesPara = document.createElement("p");
-    const readStatusLabel = document.createElement("label");
-    const readStatusInput = document.createElement("input");
-    const bookAddSubmit = document.createElement("button");
 
     // setting textContent for label
     titleLabel.textContent = "Title";
@@ -81,6 +80,10 @@ function add(){
     // adding properties for different elements
     addBookForm.method = "post";
     addBook.classList.add("book-input");
+    titleInput.classList.add("title-input");
+    authorInput.classList.add("author-input");
+    pagesInput.classList.add("pages-input");
+    pagesInput.type = "number"
     readStatusInput.type = "checkbox";
     bookAddSubmit.classList.add("book-input-button");
 
@@ -99,16 +102,22 @@ function add(){
     
     
     addButton.disabled = true;// disabling the addBook button
-
-    // pressing the submit button removes the bookInputContainer from HTML file
-    bookAddSubmit.addEventListener("click", (e)=>{
-        e.preventDefault(); // preventing form from submitting
-        container.removeChild(addBook);
-        container.style.display = null;
-        container.style.gridTemplateColumns = null;
-        container.style.backgroundColor = null;
-        addButton.disabled = false;// enables back the addBook
-    } )
 }
+function submitNewBook(e){
+    e.preventDefault(); // preventing form from submitting
+    addBookToLibrary();
+    // resets the value to null for further adding 
+    titleInput.value = "";
+    authorInput.value="";
+    pagesInput.value="";
 
+    container.removeChild(addBook);
+    container.style.display = null;
+    container.style.gridTemplateColumns = null;
+    container.style.backgroundColor = null;
+    addButton.disabled = false;// enables back the addBook
+}
+// pressing the submit button removes the bookInputContainer from HTML file
+bookAddSubmit.addEventListener("click", submitNewBook)
 
+addButton.addEventListener("click", add);// adds new section for adding book
